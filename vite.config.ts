60ad0349639c13e488f60f6a81477e230d062a74
Abhinav Raman <abhinavraman99@gmail.com>
2024-12-16 01:23:16 +0530

    Fixed build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import type { UserConfig as VitestUserConfig } from 'vitest/config';
 
-// https://vite.dev/config/
+declare module 'vite' {
+    export interface UserConfig {
+        test: VitestUserConfig['test'];
+    }
+}
+
 export default defineConfig({
     plugins: [react()],
     test: {
